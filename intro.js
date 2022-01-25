444d4556e026177054e57fc01a60a4f52f162cd7
Paarthbathla338 <Paarth.bathla@outlook.com>
2022-01-25 21:04:34 +0530

    Learning About Modules

--- intro.js
+++ intro.js
@@ -1,10 +1,30 @@
-const amount = 2
+// const amount = 2
 
-if(amount<10){
-    console.log("Amount is Smaler than 10")
-}
-else{
-    console.log("This is a Large Number")
-}
+// if(amount<10){
+//     console.log("Amount is Smaler than 10")
+// }
+// else{
+//     console.log("This is a Large Number")
+// }
 
-console.log("Hello World this is my First Node App")
+// console.log("Hello World this is my First Node App")
+
+// Tut 3
+const n="Paarth"
+const i="Kabir"
+const j="Sandeep"
+
+const sayHi=(name)=>{
+    if(name=="Paarth"){
+        console.log("Paarth is super strong")
+    if(name=="Kabir"){
+        console.log("Kabir is Paarth's Brother")
+    
+    }
+
+    if(name=="Sandeep"){
+        console.log("Sandeep is Paarth's Father")
+    }
+}
+}
+sayHi("Paarth")
--- intro2.js
+++ intro2.js
@@ -1,6 +1,5 @@
 myName="amount"
 console.log(myName)
 console.log(__dirname)
-setInterval(()=>{
-    console.log("You are Logged Out of the App")
-},1000)
+
+
