336756b9ad8e51bd6fcd12ce08ffccc7041264ed
Paarthbathla338 <Paarth.bathla@outlook.com>
2022-01-25 20:45:21 +0530

    First Node App

---
+++ app.js
@@ -0,0 +1,10 @@
+const amount = 2
+
+if(amount<10){
+    console.log("Amount is Smaler than 10")
+}
+else{
+    console.log("This is a Large Number")
+}
+
+console.log("Hello World this is my First Node App")
